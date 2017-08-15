dc1584f891818481639f58d9c1ed523b22a40171
pichza777 <pichza777@gmail.com>
2017-08-15 15:31:23 +0700

    xx

--- bot.php
+++ bot.php
@@ -2,15 +2,15 @@
 $access_token = 'aAMls5K18jevEiIZZlJ1zyu5u8gLxv7FGOYcaHak5tt2Zni2NfzWs5nfapzErLNnBsK8TlaCnHJvxBg1md67eMxWaFPl9GE/sCKzFpC0mM1ai70aKau/lF+0svFNjCWq8Zv1+RMvO4eRAVeYfoEybwdB04t89/1O/w1cDnyilFU=';
 
 $profileData = '';
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('aAMls5K18jevEiIZZlJ1zyu5u8gLxv7FGOYcaHak5tt2Zni2NfzWs5nfapzErLNnBsK8TlaCnHJvxBg1md67eMxWaFPl9GE/sCKzFpC0mM1ai70aKau/lF+0svFNjCWq8Zv1+RMvO4eRAVeYfoEybwdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '047a51ae1557c6a602e7a417d2e68182']);
-$response = $bot->getProfile('Uc1a1cdf18feed3aa6e74ec546cb19064');
-if ($response->isSucceeded()) {
-    $profile = $response->getJSONDecodedBody();
-    $profileData = $profile['displayName'];
+//$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('aAMls5K18jevEiIZZlJ1zyu5u8gLxv7FGOYcaHak5tt2Zni2NfzWs5nfapzErLNnBsK8TlaCnHJvxBg1md67eMxWaFPl9GE/sCKzFpC0mM1ai70aKau/lF+0svFNjCWq8Zv1+RMvO4eRAVeYfoEybwdB04t89/1O/w1cDnyilFU=');
+//$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '047a51ae1557c6a602e7a417d2e68182']);
+//$response = $bot->getProfile('Uc1a1cdf18feed3aa6e74ec546cb19064');
+//if ($response->isSucceeded()) {
+  //  $profile = $response->getJSONDecodedBody();
+   // $profileData = $profile['displayName'];
     //echo $profile['pictureUrl'];
     //echo $profile['statusMessage'];
-}
+//}
 
 // Get POST body content
 $content = file_get_contents('php://input');
