9b353159dfe3ef9183e9d58b068e3b750073fadb
pichza777 <pichza777@gmail.com>
2017-08-15 19:27:46 +0700

    x

--- bot.php
+++ bot.php
@@ -42,23 +42,31 @@ if (!is_null($events['events'])) {
 				// 'type' => 'text',
 				// 'text' => 'สวัสดี '. $user['displayName']//$userId//$sss //'ทดสอบ '. ' : ' .$text . ' '. $event
 				// ];
-			$messages = [{
-						 "type" => "template",
-			  "altText" => "this is a buttons template",
-			  "template"=> {
-				  "type"=> "buttons",
-				  "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-				  "title"=> "Menu",
-				  "text"=> "Please select",
-				  "actions"=> [
-					  {
-						"type"=> "postback",
-						"label"=> "Buy",
-						"data"=> "action=buy&itemid=123"
-					  },
-				  ]
-			  }
-			}
+			$messages = [ "type": "template",
+					  "altText": "this is a buttons template",
+					  "template": {
+						  "type": "buttons",
+						  "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+						  "title": "Menu",
+						  "text": "Please select",
+						  "actions": [
+							  {
+								"type": "postback",
+								"label": "Buy",
+								"data": "action=buy&itemid=123"
+							  },
+							  {
+								"type": "postback",
+								"label": "Add to cart",
+								"data": "action=add&itemid=123"
+							  },
+							  {
+								"type": "uri",
+								"label": "View detail",
+								"uri": "http://example.com/page/123"
+							  }
+						  ]
+					  }
 			]
 			
 			
