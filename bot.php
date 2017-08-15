e591a1e87aab223a05a79378bea9f4c4e7d026b7
pichza777 <pichza777@gmail.com>
2017-08-15 19:28:59 +0700

    x

--- bot.php
+++ bot.php
@@ -67,14 +67,14 @@ if (!is_null($events['events'])) {
 							  }
 						  ]
 					  }
-			]
+				]
 			
 			
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => $messages,
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
