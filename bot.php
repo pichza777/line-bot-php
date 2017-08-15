77cfc6d25ef8aa1baea497fb0d4433ae93d08aad
pichza777 <pichza777@gmail.com>
2017-08-15 19:31:25 +0700

    s

--- bot.php
+++ bot.php
@@ -42,31 +42,13 @@ if (!is_null($events['events'])) {
 				// 'type' => 'text',
 				// 'text' => 'สวัสดี '. $user['displayName']//$userId//$sss //'ทดสอบ '. ' : ' .$text . ' '. $event
 				// ];
-			$messages = [ "type": "template",
-					  "altText": "this is a buttons template",
-					  "template": {
-						  "type": "buttons",
-						  "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-						  "title": "Menu",
-						  "text": "Please select",
-						  "actions": [
-							  {
-								"type": "postback",
-								"label": "Buy",
-								"data": "action=buy&itemid=123"
-							  },
-							  {
-								"type": "postback",
-								"label": "Add to cart",
-								"data": "action=add&itemid=123"
-							  },
-							  {
-								"type": "uri",
-								"label": "View detail",
-								"uri": "http://example.com/page/123"
-							  }
-						  ]
-					  }
+			$messages = [ 'type' => 'template',
+				  'altText' => 'this is a buttons template',
+				  'template' => {
+					  'type':=> 'buttons',
+					  'thumbnailImageUrl'=> 'https://example.com/bot/images/image.jpg',
+					  'title'=> 'Menu',
+					
 				]
 			
 			
@@ -74,7 +56,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => $messages,
+				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
